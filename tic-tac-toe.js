c371abd1ceaae126fbc4055f1b450e220c2d6dda
tia <tiakristensalmon@gmail.com>
2022-10-14 08:12:51 -0600

    Updated js to check for the winner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,24 @@
 window.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('#board');
     const squares = Array.from(container.children);
+    const status = document.querySelector('#status');
     const BOARD_LENGTH = 9;
 
     let board = [[],[]];
     let currentPlayer = 'X';
+    let gameInProgress = true; // flag to keep track of whether or not game has ended
+
+    // All possible combinations to win:
+    const winningConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
     squares.forEach((square, index) => {
         // Adding squares to screen
@@ -20,7 +34,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     // If there's nothing in the HTML play otherwise leave it as is
     const changePlayerOnClick = (square, index) => {
-        if(!square.innerText) play(square, index);
+        if(!square.innerText && gameInProgress) play(square, index);
     }; 
 
     // Play either X or O and add to board array
@@ -29,6 +43,28 @@ window.addEventListener('DOMContentLoaded', () => {
         square.classList.add(`${currentPlayer}`);
         (currentPlayer == 'X') ? board[0].push(index) : board[1].push(index);
         currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+        checkWinner();
     }
+
+    // Check if any player meets conditions to win the game
+    const checkWinner = () => {
+        winningConditions.forEach((winningCondition, index) => {
+            if (winningCondition.every(el => board[0].includes(el))) {
+                gameInProgress = false;
+                status.innerHTML = 'Congratulations! X is the Winner!';
+                status.classList.add('you-won');
+            }
+            else if (winningCondition.every(el => board[1].includes(el))) {
+                gameInProgress = false;
+                status.innerHTML = 'Congratulations! O is the Winner!';
+                status.classList.add('you-won');
+            } 
+            else if (board[0].length + board[1].length == BOARD_LENGTH){
+                gameInProgress = false;
+                status.innerHTML = 'There was a DRAW!';
+                status.classList.add('you-won');
+            }
+        });
+    } 
 
 });
